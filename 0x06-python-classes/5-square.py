5246c57d34fb2b4631205cdf1b74198f6fb35346
jemal1444 <jemal.eyasu@yahoo.com>
2023-06-27 15:49:29 +0000

    his is an ALX project on 0x06-python-classes batch of 2023 task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class Square:
     """
@@ -30,6 +32,15 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
